b1f6589e0a9cf1c144aef0e19948f9b045e7576c
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-01 16:03:19 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.valid? == true
+    if valid?
     sender.balance -= amount
     receiver.balance += amount
     @status = "complete"
