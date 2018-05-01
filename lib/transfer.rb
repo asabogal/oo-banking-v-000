f34a491e191f92865b76c894253b8680034a1d4b
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-01 15:33:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+
+  attr_accessor :bank_account, :transfer, :sender, :status, :amount
+  attr_reader :receiver
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    @status = "complete"
+  end
+
 end
